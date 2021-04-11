b113e3ce9b3a76838ffc4cb3ae6e3b79cce716c0
Ubuntu <ubuntu@ip-172-31-5-188.ap-northeast-1.compute.internal>
2021-04-11 16:05:05 +0000

    DRY

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,11 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
   def index
     @messages = Message.all
   end
   
   def show
-    @message = Message.find(params[:id])
+    set_message
   end
   
   def new
@@ -24,11 +25,11 @@ class MessagesController < ApplicationController
   end
   
   def edit
-    @message = Message.find(params[:id])
+    set_message
   end
   
   def update
-    @message = Message.find(params[:id])
+    set_message
     
     if @message.update(message_params)
       flash[:success] = "メッセージを更新しました。"
@@ -40,7 +41,7 @@ class MessagesController < ApplicationController
   end
   
   def destroy
-    @message = Message.find(params[:id])
+    set_message
     @message.destroy
     
     flash[:success] = "メッセージが正常に削除されました。"
@@ -48,6 +49,10 @@ class MessagesController < ApplicationController
   end
   
   private
+  def set_message
+    @message = Message.find(params[:id])
+  end
+  
   def message_params
     params.require(:message).permit(:content)
   end
